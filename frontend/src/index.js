8f10a6c9508bf30345517aae886dcc98cd3c1b57
Suwanshree Acharya <suwanshree@gmail.com>
2022-06-14 13:01:10 +0100

    Search bar and context added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { ModalProvider } from "./context/Modal";
-
+import { SearchProvider } from "./context/Search";
 import configureStore from "./store";
 import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from "./store/session";
@@ -24,13 +24,15 @@ if (process.env.NODE_ENV !== "production") {
 
 function Root() {
   return (
-    <Provider store={store}>
-      <ModalProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ModalProvider>
-    </Provider>
+    <SearchProvider>
+      <Provider store={store}>
+        <ModalProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ModalProvider>
+      </Provider>
+    </SearchProvider>
   );
 }
 
